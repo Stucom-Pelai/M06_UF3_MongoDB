d9737c3e5fbb4b0b769f2b7c519de3b17587e57d
Japoivvb <jose.portugal@stucom.com>
2025-02-03 00:38:52 +0100

    adds more operations

--- src/main/java/com/mongodb/CRUD.java
+++ src/main/java/com/mongodb/CRUD.java
@@ -1,6 +1,8 @@
 package com.mongodb;
 
 import org.bson.Document;
+import org.bson.types.ObjectId;
+
 import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Updates.combine;
 import static com.mongodb.client.model.Updates.set;
@@ -11,51 +13,73 @@ import com.mongodb.client.result.UpdateResult;
 
 import org.bson.Document;
 
-public class CRUD{
-	
+public class CRUD {
+
 	MongoCollection<Document> collection;
+	ObjectId id;
 
 	public CRUD() {
 		String uri = "mongodb://localhost:27017";
-        MongoClientURI mongoClientURI = new MongoClientURI(uri);
-        MongoClient mongoClient = new MongoClient(mongoClientURI);
+		MongoClientURI mongoClientURI = new MongoClientURI(uri);
+		MongoClient mongoClient = new MongoClient(mongoClientURI);
 
-        MongoDatabase mongoDatabase = mongoClient.getDatabase("CrudDB");
-        collection = mongoDatabase.getCollection("test");
+		MongoDatabase mongoDatabase = mongoClient.getDatabase("CrudDB");
+		collection = mongoDatabase.getCollection("test");
 	}
 
 	/**
-	 * create a new document in test collection 
+	 * create a new document in test collection
 	 */
 	public void create() {
-		Document document = new Document().append("firstName", "Upanshu").append("lastName", "Chaudhary")
+		Document document = new Document("_id", new ObjectId()).append("firstName", "Carlos").append("lastName", "Lopez")
+				.append("age", "42").append("gender", "Male").append("designation", "Programmer");
+		collection.insertOne(document);
+		System.out.println("document inserted" + document);
+		this.id = new ObjectId();
+		document = new Document("_id", this.id).append("firstName", "John").append("lastName", "Doe")
 				.append("age", "22").append("gender", "Male").append("designation", "Software Consultant");
 		collection.insertOne(document);
+		System.out.println("document inserted with ID: " + this.id);
 	}
 
 	/**
 	 * read a document from test collection
 	 */
 	public void read() {
+		// read document by position
 		Document document = collection.find().first();
-		System.out.println(document.toJson());
+		System.out.println("document read by position " + document.toJson());
+		
+		// read document by id
+		document = collection.find(eq("_id", this.id)).first();
+		System.out.println("document read by id " + document.toJson());
+		
+		// read n documents
+		// Read the first '5' documents
+	    Iterable<Document> documents = collection.find().limit(5);
+
+	    int count = 0;
+	    for (Document doc : documents) {
+	        System.out.println("document read on list " + (++count) + ": " + doc.toJson());
+	    }
 	}
 
 	/**
 	 * update a document from test collection
 	 */
 	public void update() {
-		UpdateResult result = collection.updateOne(eq("firstName", "Upanshu"), combine(set("age", "23"), set("lastName", "Singh")));		
-		System.out.println(result.toString());
+		UpdateResult result = collection.updateOne(eq("firstName", "John"),
+				combine(set("age", "23"), set("lastName", "Smith")));
+		System.out.println("document updated by name" + result.toString());
 	}
 
 	/**
 	 * delete a document from test collection
 	 */
 	public void delete() {
-		DeleteResult result = collection.deleteMany(eq("age", "22"));
-		System.out.println(result.toString());
-		
+		DeleteResult result = collection.deleteMany(eq("age", "42"));
+		System.out.println("document deleted by name" + result.toString());
+
 	}
 
 }
